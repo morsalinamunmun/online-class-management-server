353da6764487a7304599b468be1093b36c33816d
morsalinamunmun <morsalinamunmun@gmail.com>
2023-11-29 09:06:41 +0600

    jwt api & user update admin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+var jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,8 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASS)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ddlqajr.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -31,7 +30,17 @@ async function run() {
     const userCollection = client.db("classManagement").collection("users");
     const applicationCollection = client.db("classManagement").collection("teacherRequest");
     
+    //jwt related api
+    app.post('/jwt', async(req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.TOKEN_ACCESS, {expiresIn: '1h'});
+      res.send({token});
+    })
     //user
+    app.get('/users', async(req,res)=>{
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
     app.post('/users', async(req, res) =>{
       const user = req.body;
       const query = {email: user.email}
@@ -40,6 +49,27 @@ async function run() {
         return res.send({message: 'user Already add', insertedId: null})
       }
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    //user adm
+    app.patch('/users/admin/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    //user delete
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     })
 
